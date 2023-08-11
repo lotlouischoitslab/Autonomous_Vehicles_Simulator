9b2446a6295754db598b546d5365473451718af2
Louis Sungwoo Cho <louis.s.cho@gmail.com>
2023-08-11 15:56:56 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+'''
+Project: Autonomous Vehicles Simulator using Deep Reinforcement Learning
+Author: Louis Sungwoo Cho
+Date Created: 5/22/2023
+'''
+
+from src.utils import DeepQNetwork 
+
+def main():
+    print('Hello World!')
+    louis = DeepQNetwork(lr, input_dims, fc1_dims, fc2_dims, n_actions)
+
+
+if __name__ =='__main__':
+    main()
